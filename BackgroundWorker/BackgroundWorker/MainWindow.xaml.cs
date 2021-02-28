3d3708bcbcf88d030b7503a7aa5bf96bea6bfd02
Vyacheslav Lasukov <lasukovv902@gmail.com>
2021-02-28 15:41:00 +0300

    Second Lab

---
+++ BackgroundWorker/BackgroundWorker/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace BackgroundWorker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public System.ComponentModel.BackgroundWorker aWorker;
+
+        private delegate void UpdateDelegate(int i);
+
+        private void UpdateLabel(int i)
+        {
+            label1.Content = "Cycles: " + i.ToString();
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.aWorker = new System.ComponentModel.BackgroundWorker();
+            aWorker.WorkerSupportsCancellation = true;
+
+            aWorker.DoWork += aWorker_DoWork;
+            aWorker.RunWorkerCompleted += aWorker_RunWorkerCompleted;
+        }
+
+        private void aWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                for (int j = 1; j <= 10000000; j++)
+                {
+                }
+                if (aWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                UpdateDelegate update = new UpdateDelegate(UpdateLabel);
+                label1.Dispatcher.BeginInvoke(DispatcherPriority.Normal, update, i);
+            }
+        }
+        private void aWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                label2.Content = "Run Completed";
+
+            }
+            else
+            {
+                label2.Content = "Run Cancelled";
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            aWorker.RunWorkerAsync();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            aWorker.CancelAsync();
+        }
+    }
+}
---
+++ WpfUserControl/WpfUserControl/ShowNumberControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System;
+
+namespace WpfUserControl
+{
+    /// <summary>
+    /// Interaction logic for ShowNumberControl.xaml
+    /// </summary>
+    public partial class ShowNumberControl : UserControl
+    {
+        private int currNumber = 0;
+
+        public int CurrentNumber
+        {
+            get { return (int)GetValue(CurrentNumberProperty); }
+            set { SetValue(CurrentNumberProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentNumberProperty =
+            DependencyProperty.Register(
+                "CurrentNumber", 
+                typeof(int), 
+                typeof(ShowNumberControl),
+                new UIPropertyMetadata(100, new PropertyChangedCallback(CurrentNumberChanged)),
+                new ValidateValueCallback(ValidateCurrentNumber));
+
+        public ShowNumberControl()
+        {
+            InitializeComponent();
+        }
+
+        public static bool ValidateCurrentNumber(object value)
+        {
+            if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static void CurrentNumberChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ShowNumberControl s = (ShowNumberControl)depObj;
+            Label theLabel = s.numberDisplay;
+            theLabel.Content = args.NewValue.ToString();
+        }
+    }
+}
